18a1a0bbd096971c7c1e4f105736cbad32471dbe
Vlad <vvladxx@gmail.com>
2011-03-13 13:57:16 +0300

    Test http-keepalive-max-conn-per-client

--- tests/keep_alive.py
+++ tests/keep_alive.py
@@ -35,6 +35,14 @@ def run_test():
 	response_line, headers, body = test.do(request_headers)
 	
 	for (k, v) in headers:
-		if k.lower() == 'connection' and v.lower().find('keep-alive') != -1:
-			return
-	raise UnitTestError()
+		if k.lower() == 'connection' and v.lower().find('keep-alive') == -1:
+			raise UnitTestError()
+	
+	test = range(1000)
+	for i in xrange(1000):
+		test[i] = Tests()
+		response_line, headers, body = test[i].do(request_headers)
+		for (k, v) in headers:
+			if k.lower() == 'connection' and v.lower().find('keep-alive') == -1:
+				print('Keep-Alive max connections: %d' % i)
+				return
