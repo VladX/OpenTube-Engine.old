cb0888a863f1190d63cca077e828ffe72d2c5378
Vlad <vvladxx@gmail.com>
2011-09-28 00:18:44 +0400

    Removed ctpp2

--- libs/ctpp2/ClearPackage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -f Makefile ]; then
-    make clean
-fi
-
-rm -fr CMakeCache.txt CMakeFiles DartTestfile.txt Makefile Testing cmake_install.cmake progress.make include/CTPP2SysHeaders.h doc/html *.ct2 install_manifest.txt build-stamp configure-stamp debian/ctpp2 debian/ctpp2-dev debian/tmp debian/ctpp2.postinst.debhelper debian/ctpp2.postrm.debhelper debian/ctpp2.substvars debian/dirs debian/files ctpp2-config CTestTestfile.cmake ctpp2-config
