ad48aef119cb01ff5bb2d7b503a42339554f2ad4
Vlad <vvladxx@gmail.com>
2011-10-10 16:33:32 +0400

    GNU GPL header updated

--- src/file_temporary_storage.h
+++ src/file_temporary_storage.h
@@ -1,11 +1,11 @@
 /*
  * This file is part of Opentube - Open video hosting engine
  *
- * Copyright (C) 2011 - VladX; http://vladx.net/
+ * Copyright (C) 2011 - Xpast; http://xpast.me/; <vvladxx@gmail.com>
  *
  * Opentube is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * Opentube is distributed in the hope that it will be useful,
@@ -14,9 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Opentube; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA  02110-1301  USA
+ * along with Opentube.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef FILE_TEMPORARY_STORAGE_H
